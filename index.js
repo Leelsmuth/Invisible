41b46cc731a3800defd692b8d9f52360044f7214
Leelsmuth <femianjorin@rocketmail.com>
2020-06-30 22:13:23 +0100

    MVP completed, returns temp and time for location

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log('Testing this app')
+const request = require("request");
+const dotenv = require("dotenv").config();
+
+const address = process.argv[2];
+
+const url = `http://api.openweathermap.org/data/2.5/weather?q=${address}&units=metric&appid=${process.env.API_KEY}`;
+
+const getTimeFromTimezoneOffset = (offset) => {
+
+  // create Date object for current location
+  const localDate = new Date();
+  const localTime = localDate.getTime();
+
+  const localOffset = localDate.getTimezoneOffset() * 60000;
+
+  const utcTime = localTime + localOffset;
+
+  const adjustedTime = utcTime + offset * 1000;
+  return new Date(adjustedTime).toLocaleTimeString("en-US");
+};
+
+
+if (!address) {
+  return console.log("Please enter the name of the city");
+}
+
+request(url, (error, response, body) => {
+  try {
+    const data = JSON.parse(body);
+    const time = getTimeFromTimezoneOffset(data.timezone);
+    const temp = data.main.temp;
+    console.log(`It's currently ${temp}°C in ${data.name} and the time is ${time}`);
+  }
+  catch (error) {
+    console.log(error);
+  }
+});
